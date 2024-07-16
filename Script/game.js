d51a194e9bdd6af7496935f89dc4bdc922b9f291
Salma-se2005204 <se2005204@qu.edu.qa>
2024-07-17 00:58:44 +0300

    MCQ check implemented + Created sort words question + added more data to JSON

--- Script/game.js
+++ Script/game.js
@@ -1,6 +1,7 @@
 let state = {
     cardIndex: 0,
-    lvlIndex: 0
+    lvlIndex: 0,
+    checkAnswer: false
 }
 
 function setState(newState,data) {
@@ -8,6 +9,23 @@ function setState(newState,data) {
 
     render(data);
 }
+
+function onDragStart(event) {
+    event.dataTransfer.setData('text/plain', event.target.id);
+    event.currentTarget.style.backgroundColor = 'yellow';
+}
+
+function onDragOver(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    const id = event.dataTransfer.getData('text');
+    const draggableElement = document.getElementById(id);
+    const dropzone = event.target;
+    dropzone.appendChild(draggableElement);
+    event.dataTransfer.clearData();
+  }
 
 function render(data) {
     var cardHTML = ``
@@ -48,27 +66,88 @@ function render(data) {
                 <div>
         </div>
         `
+        state = {
+            cardIndex: state.cardIndex,
+            lvlIndex: state.lvlIndex,
+            checkAnswer: true
+        }
+
+    } else if(data[state.lvlIndex].type=="sort"){
+        const choicesHTML = data[state.lvlIndex].elements[state.cardIndex].labels.map((c, i) => `
+                <div id="draggable-${i}" class="draggable-label" draggable="true" ondragstart="onDragStart(event);">
+                    ${c.txt}
+                </div>
+            `).join('');
+
+        const sortedHTML = data[state.lvlIndex].elements[state.cardIndex].answer.map((c, i) => `
+            <div id="zone-${i}" class="label-dropzone" ondragover="onDragOver(event);" ondrop="onDrop(event);"></div>
+            `).join('');
+
+        cardHTML = `
+            <h1>${data[state.lvlIndex].title}</h1>
+            <h4>${data[state.lvlIndex].desc}</h4>
+            <div class="sort-area">
+                    <div class="labels-to-sort">
+                    ${choicesHTML}
+                    </div>
+                    <div class="sorted-labels">
+                    ${sortedHTML}
+                    </div>
+            </div>
+            `
+
+        state = {
+            cardIndex: state.cardIndex,
+            lvlIndex: state.lvlIndex,
+            checkAnswer: true
+        }
     }
 
     document.getElementById('game-area').innerHTML = cardHTML
+}
+
+function checkAnswer(data){
+    if(data[state.lvlIndex].type=="MCQ"){
+        var answer = document.querySelector(`input[name="choice"][value="${data[state.lvlIndex].elements[state.cardIndex].answer}"]`);
+        var label = document.querySelector(`label[for="${answer.id}"]`);
+        label.style.backgroundColor = "lightgreen"
+
+        let selectedChoice = document.querySelector('input[name="choice"]:checked');
+
+        if(selectedChoice!= null && answer.id!=selectedChoice.id){
+            var wrongLabel =document.querySelector(`label[for="${selectedChoice.id}"]`);
+            wrongLabel.style.backgroundColor = "lightsalmon"
+        }
+    }
+
+    state = {
+        cardIndex: state.cardIndex,
+        lvlIndex: state.lvlIndex,
+        checkAnswer: false
+    }
 }
 
 fetch("https://salma-eletreby.github.io/Restaurante-Espanol/Data/spanishFood.json")
 .then((response) => response.json())
 .then((data) => {
     document.getElementById("next").onclick = () => {
-        if(state.cardIndex+1 == data[state.lvlIndex].elements.length){
+        if(state.checkAnswer == true){
+            checkAnswer(data)
+        }
+        else if(state.cardIndex+1 == data[state.lvlIndex].elements.length){
             setState(
                 { 
                     cardIndex: 0,
-                    lvlIndex: ++state.lvlIndex
+                    lvlIndex: ++state.lvlIndex,
+                    checkAnswer: false
                 },
                 data)
         } else{
             setState(
                 { 
                     cardIndex: ++state.cardIndex,
-                    lvlIndex: state.lvlIndex
+                    lvlIndex: state.lvlIndex,
+                    checkAnswer: false
                 },
                 data)
         }
